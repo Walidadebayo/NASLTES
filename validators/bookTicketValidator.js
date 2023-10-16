055bf429575202d2e128a701bd4514a377588ea6
Walid Adebayo <adebayowalid@gmail.com>
2023-10-16 11:04:02 +0100

    ticket validator

--- validators/bookTicketValidator.js
+++ validators/bookTicketValidator.js
@@ -3,9 +3,9 @@ const Admin = require('../models/admin');
 const checkForValidationErrors = require('../middlewares/checkForValidationErrors');
 
 let bookTicketValidators = [
-    body('full_name').notEmpty().withMessage('Please enter your name'),
-    body('student_email').notEmpty().withMessage('Please enter email address').isEmail().withMessage('Please enter valid email address'),
-    body('phone').notEmpty().withMessage('Please enter phone number').isMobilePhone('en-NG').withMessage('Please enter valid phone number'),
+    body('full_name').notEmpty().withMessage('Please enter your name').trim(),
+    body('student_email').notEmpty().withMessage('Please enter email address').isEmail().withMessage('Please enter valid email address').trim(),
+    body('phone').notEmpty().withMessage('Please enter phone number').isMobilePhone('en-NG').withMessage('Please enter valid phone number').trim(),
     body('type').custom((value)=>{
         if(!['regular'].includes(value)){
             throw new Error('Please select ticket type')
