368ad08de02d01f70bf6418ba2939eea667cf010
Walid Adebayo <adebayowalid@gmail.com>
2023-10-16 11:43:49 +0100

    email

--- models/cancelledPayment.js
+++ models/cancelledPayment.js
@@ -69,7 +69,7 @@ function cancelledPayment(email, name, domain) {
                             </tr>
                             <tr>
                                 <td style="text-align:center;">
-                                    <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>CDF</strong></p>
+                                    <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>Adeal</strong></p>
                                 </td>
                             </tr>
                             <tr>
